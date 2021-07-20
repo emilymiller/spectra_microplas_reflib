a90934f0ffffb9c938acf02e69eb8cb51242d6e3
emilymiller <eamiller@ucdavis.edu>
2021-07-20 11:25:44 -0700

    Producing spectra plots on multiple pages for both reference library and microparticles.

--- cleaning_code.R
+++ cleaning_code.R
@@ -47,6 +47,8 @@ df$raman_shift<-df[,1]
 
 dev.off()
 
+# single page of all plots by sample (both wave numbers
+# for a given sample are on one plot)
 ggplot(df, aes(x=raman_shift, y=INT,
                color=wave_number)) + 
   geom_line()+ facet_wrap(.~sample_id,scales="free")+
@@ -54,39 +56,96 @@ ggplot(df, aes(x=raman_shift, y=INT,
   ylab("Intensity")+
   themeo + theme(text=element_text(size=10))
 
-### split into two pages for size
+### Split into multiple pages for size and separated
+### samples that were run at both wave numbers (532 & 780nm) 
+### into two plots. Axes are different.
 
 #install.packages("ggforce")
 library(ggforce)
 
-# Pagination: page 1
+# Pagination: page 1 example
 df$wave_number<-as.factor(df$wave_number)
 ggplot(df, aes(x=raman_shift, y=INT,
                color=wave_number)) + 
   geom_line()+ facet_wrap_paginate(sample_id~wave_number,scales="free",
                                    ncol = 4, nrow = 4,
-                                   page = 1)+
+                                   page = 3)+
   xlab("Raman Shift / cm-1")+
   ylab("Intensity")+
   themeo + theme(text=element_text(size=10))
 
-
-#* start by doing separate paneled plot for 532 and 780
-#(so that the axes are dif)
-
+reflib<-df
 #dev.off()
 
+
+##########################################
+#
+#* then do same for the sediment data
+#
+##########################################
+
+
+
+setwd("C:/Users/emily/Documents/MBARI/spectra")
+
+list_of_files <- list.files(path = "./S&N Labs results/MBA Raman Project Shipment #2/MBA Microparticles (Red Box)",
+                            recursive = TRUE,
+                            pattern = "\\.csv$",
+                            full.names = TRUE)
+df<-list_of_files %>% set_names() %>%
+  map_df(read_csv,skip=1,.id="file_name")
+df<-as.data.frame(df)
+head(df)
+
+df$wave_number1 = str_sub(df$file_name,-14)
+df$wave_number <- str_sub(df$wave_number1,1,7)
+df$wave_number1 <- NULL
+df$wave_number <- str_sub(df$wave_number,-3)
+
+df$sample_id1 <- str_sub(df$file_name,-14)
+df$sample_id <-str_sub(df$sample_id1,1,3)
+df$sample_id1<-NULL
+
+df$file_name <-NULL
+
+head(df)
+
+df$raman_shift<-df[,1]
+
+dev.off()
+
+# single page of all plots by sample (both wave numbers
+# for a given sample are on one plot)
 ggplot(df, aes(x=raman_shift, y=INT,
                color=wave_number)) + 
   geom_line()+ facet_wrap(.~sample_id,scales="free")+
   xlab("Raman Shift / cm-1")+
   ylab("Intensity")+
+  themeo + theme(text=element_text(size=6))
+
+# needs redo for sample ids with duplicate numbers (# 050)
+
+### Split into multiple pages for size and separated
+### samples that were run at both wave numbers (532 & 780nm) 
+### into two plots. Axes are different.
+
+#install.packages("ggforce")
+library(ggforce)
+
+# Pagination: page 1 example
+df$wave_number<-as.factor(df$wave_number)
+ggplot(df, aes(x=raman_shift, y=INT,
+               color=wave_number)) + 
+  geom_line()+ facet_wrap_paginate(sample_id~wave_number,scales="free",
+                                   ncol = 4, nrow = 4,
+                                   page = 4)+
+  xlab("Raman Shift / cm-1")+
+  ylab("Intensity")+
   themeo + theme(text=element_text(size=10))
 
+micropart<-df
 
-
-#* then do same for the sediment data
-
+#dev.off()
 
 
 
@@ -94,4 +153,9 @@ ggplot(df, aes(x=raman_shift, y=INT,
 
 #* go through tyler's code
 
+# add a source column and material column or merge with database
+
+
 #* how to combine the two wave numbers per sample
+
+# pearson's distance
