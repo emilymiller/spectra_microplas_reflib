b557d03a471466be0e59dbbcd0de08986b37de2c
emilymiller <eamiller@ucdavis.edu>
2022-01-09 18:57:36 -0800

    Commenting and including section formally in reflib_fig1.R so that both scripts run independently.

--- cleaning_code.R
+++ cleaning_code.R
@@ -203,10 +203,197 @@ rm(reflib)
 rm(reflib214)
 
 
-
-# use reflib_source for preprocessing and eventually
-# matching protocol in next scripts 
-
 write.csv(reflib_source,file="reflib_source.csv") 
-# -> go to reflib_fig1.R 
 
+
+############################################
+
+# add category column
+# biological polymers, pristine plastic polymers,
+# wild plastic polymers
+
+# make only one row per sample_id
+category_counts <- reflib_source[!duplicated(reflib_source$sample_id),]
+unique(category_counts$parent_grp)
+
+# biological then biological
+
+# pristine plastic =
+# industrial
+# bioplastic
+# consumer (+ new in estimated age)
+# consumer (+ post purchase new in estimated age)
+# building material 
+# boating
+# container (new)
+# electronics
+# adhesive
+# fiber
+# upholstery
+
+# wild plastic = 
+# fishery
+# agricultural
+# beachcast
+# consumer (+used)
+# consumer (+used kids toy)
+# container (used)
+# oml
+
+category_counts$category<-ifelse(category_counts$parent_grp=="biological","biological",
+                          ifelse(category_counts$parent_grp=="fishery","wild_plastic",
+                          ifelse(category_counts$parent_grp=="consumer" & category_counts$estimated_age=="used","wild_plastic",
+                          ifelse(category_counts$parent_grp=="consumer" & category_counts$estimated_age=="used kids toys","wild_plastic",
+                          ifelse(category_counts$parent_grp=="container" & category_counts$estimated_age=="used","wild_plastic",
+                         ifelse(category_counts$parent_grp=="agricultural", "wild_plastic",
+                      ifelse(category_counts$parent_grp=="beachcast", "wild_plastic",
+                      ifelse(category_counts$parent_grp=="oml", "wild_plastic", "pristine_plastic"))))))))
+
+head(category_counts)
+library(plyr)
+head(category_counts)
+category_counts$gen_poly
+category_counts$gen_poly<-as.character(category_counts$gen_poly)
+# 
+
+# rename biological polymers by tissue type
+# chitin
+# cellulose
+# dentin
+# calcium carbonate
+# keratin
+# bone
+class(category_counts$gen_poly)
+category_counts$gen_poly
+category_counts$gen_poly<-ifelse(category_counts$gen_poly==
+                                   "Zostera eelgrass blade","cellulose",category_counts$gen_poly)
+category_counts$gen_poly<-ifelse(category_counts$gen_poly==
+                                   "tiger shrimp carapace","chitin",category_counts$gen_poly)
+category_counts$gen_poly<-ifelse(category_counts$gen_poly==
+                                   "surf clam shell","calcium carbonate",category_counts$gen_poly)
+category_counts$gen_poly<-ifelse(category_counts$gen_poly==
+                                   "squid beaks, albatross stomach","chitin",category_counts$gen_poly)
+category_counts$gen_poly<-ifelse(category_counts$gen_poly==
+                                   "sea otter tooth","dentin",category_counts$gen_poly)
+category_counts$gen_poly<-ifelse(category_counts$gen_poly==
+                                   "rhino auklet upper mandible horn","bone",category_counts$gen_poly)
+category_counts$gen_poly<-ifelse(category_counts$gen_poly==
+                                   "Pacific purple urchin spines; Strongylocentrotus purpuratus; from otter female 613","bone",category_counts$gen_poly)
+category_counts$gen_poly<-ifelse(category_counts$gen_poly==
+                                   "olive ridley sea turtle scapula condyle","bone",category_counts$gen_poly)
+category_counts$gen_poly<-ifelse(category_counts$gen_poly==
+                                   "dungeness crab carapace","chitin",category_counts$gen_poly)
+category_counts$gen_poly<-ifelse(category_counts$gen_poly==
+                                   "Macrocystis kelp blade","algin",category_counts$gen_poly)
+category_counts$gen_poly<-ifelse(category_counts$gen_poly==
+                                   "sea otter maxilla","bone",category_counts$gen_poly)
+category_counts$gen_poly<-ifelse(category_counts$gen_poly==
+                                   "Laysan albatross feather","keratin",category_counts$gen_poly)
+category_counts$gen_poly<-ifelse(category_counts$gen_poly==
+                                   "market squid bell","myofibrillar protein",category_counts$gen_poly)
+category_counts$gen_poly<-ifelse(category_counts$gen_poly==
+                                   "green mussel shell","calcium carbonate",category_counts$gen_poly)
+category_counts$gen_poly<-ifelse(category_counts$gen_poly==
+                                   "Chinook salmon muscle","myofibrillar protein",category_counts$gen_poly)
+category_counts$gen_poly<-ifelse(category_counts$gen_poly==
+                                   "Chinook salmon skin","collagen",category_counts$gen_poly)
+category_counts$gen_poly<-ifelse(category_counts$gen_poly==
+                                   "acrylic","PMMA",category_counts$gen_poly)
+category_counts$gen_poly<-ifelse(category_counts$gen_poly==
+                                   "Chinook salmon skin","collagen",category_counts$gen_poly)
+category_counts$gen_poly<-ifelse(category_counts$gen_poly==
+                                   "blue cotton towel","cellulose",category_counts$gen_poly)
+category_counts$gen_poly<-ifelse(category_counts$gen_poly==
+                                   "Choy et al 2019 gear #8","cellulose",category_counts$gen_poly)
+
+category_counts$category<-as.factor(category_counts$category)
+
+# create column that is a combo of polymer-category
+# then pull unique
+
+category_counts$polymer_category <- paste(category_counts$gen_poly,
+                                          category_counts$category,
+                                          sep="-")
+head(category_counts)
+sort(unique(category_counts$polymer_category))
+
+
+head(reflib_source)
+names(reflib_source)
+names(category_counts)
+head(category_counts) # want to add gen_poly and category back into 
+# reflib_source
+colnames(category_counts)[14]<-"polymer_material"
+library(dplyr)
+category_counts2<-category_counts %>%
+  select(sample_id, polymer_material, category)
+head(category_counts2)
+head(reflib_comb)
+reflib_categories<-merge(reflib_comb,category_counts2,by="sample_id",
+                         all.x=T)
+head(reflib_categories)
+unique(reflib_categories$polymer_material)
+unique(reflib_categories$category)
+
+######################################################
+#
+#cleaning
+#
+######################################################
+
+rm(df3)
+rm(data)
+rm(df2)
+rm(histo)
+rm(reflib_comb)
+rm(reflib_source)
+rm(category_counts2)
+
+######################################################
+#
+# correcting wave number in Shipment #1 specimens
+# (they were labeled in original pdf S&N sent, not in 
+# data files)
+#
+#####################################################
+
+class(reflib_categories$wave_number)
+reflib_categories$wave_number<-as.character(reflib_categories$wave_number)
+
+reflib_categories$wave_number<-ifelse(reflib_categories$wave_number==
+                                        "PLAS157","785",reflib_categories$wave_number)
+head(reflib_categories)
+reflib_categories$wave_number<-ifelse(reflib_categories$wave_number==
+                                        "PLAS115","785",reflib_categories$wave_number)
+reflib_categories$wave_number<-ifelse(reflib_categories$wave_number==
+                                        "PLAS117","785",reflib_categories$wave_number)
+reflib_categories$wave_number<-ifelse(reflib_categories$wave_number==
+                                        "PLAS163","785",reflib_categories$wave_number)
+reflib_categories$wave_number<-ifelse(reflib_categories$wave_number==
+                                        "PLAS032","785",reflib_categories$wave_number)
+reflib_categories$wave_number<-ifelse(reflib_categories$wave_number==
+                                        "PLAS175","785",reflib_categories$wave_number)
+reflib_categories$wave_number<-ifelse(reflib_categories$wave_number==
+                                        "BIOL006","785",reflib_categories$wave_number)
+reflib_categories$wave_number<-ifelse(reflib_categories$wave_number==
+                                        "BIOL013","785",reflib_categories$wave_number)
+
+head(reflib_categories)
+
+
+#######################################################
+
+write.csv(reflib_categories,file="reflib_categories.csv")
+
+
+###################################################
+
+###################################################
+
+## # use reflib_categories for preprocessing and eventually
+# matching protocol in next scripts
+# -> go to preprocessing to continue with data analyses
+
+#-> or can also go to reflib_fig1.R for paper fig construction
+
+#######################################################
